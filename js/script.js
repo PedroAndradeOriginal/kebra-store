422c9a4f19ac3c016c42cdcdc0d24f563e0d92f3
Pedro Mauricio De Almeida Chagas <pedromauricio360@gmail.com>
2022-12-08 16:58:33 -0300

    Feat:Criando animação dos cards

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let card = document.querySelector('.cardimg');
+let modal = document.querySelector('.slideModelo');
+let botao = document.querySelector('#botao-fechar');
+
+
+card.addEventListener('click', abreModal, false);
+
+function abreModal() {
+    modal.classList.toggle('modal-troca')
+}
+
+botao.addEventListener('click', fechaModal, false);
+
+function fechaModal() {
+    modal.classList.toggle('modal-troca')
+}
