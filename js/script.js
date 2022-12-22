ba8ad5cdc34fe67501d34d6f0d13567c607b2395
Pedro Andrade <pedrofreitasdeandrade@gmail.com>
2022-12-21 21:29:49 -0300

    Finalizando o JS

--- js/script.js
+++ js/script.js
@@ -1,50 +1,44 @@
-let modelos = document.querySelectorAll('img');
-let modalCard = document.querySelectorAll('.slideModelo')
-let botao = document.querySelector('#botao-fechar');
+// declarando os arrays
 
-for(let i=0; i<modelos.length; i++){
-    
-    modelos[i].addEventListener('click', funcaoPrincipal);
-    
-    function funcaoPrincipal() {
-        for (let x=0; x<modalCard.length; x++){
-            console.log(i);
-            if(i==2) {
-                modalCard[0].classList.toggle('modal-troca');
-            }
-            if(i==7) {
-                modalCard[1].classList.toggle('modal-troca');
-            }
-            if(i==12) {
-                modalCard[2].classList.toggle('modal-troca');
-            }
-            if(i==17) {
-                modalCard[3].classList.toggle('modal-troca');
-            }
-            if(i==22) {
-                modalCard[4].classList.toggle('modal-troca');
-            }
-            if(i==27) {
-                modalCard[5].classList.toggle('modal-troca');
-            }
-            if(i==32) {
-                modalCard[6].classList.toggle('modal-troca');
-            }
-            if(i==37) {
-                modalCard[7].classList.toggle('modal-troca');
-            }
-            if(i==42) {
-                modalCard[8].classList.toggle('modal-troca');
-            }
-            if(i==47) {
-                modalCard[9].classList.toggle('modal-troca');
-            }
+const cards = [];
+cards.push(document.getElementById("card01"), document.getElementById("card02"), document.getElementById("card03"), document.getElementById("card04"), document.getElementById("card05"), document.getElementById("card06"), document.getElementById("card07"), document.getElementById("card08"), document.getElementById("card09"));
+const modals = [];
+modals.push(document.getElementById("modal01"), document.getElementById("modal02"), document.getElementById("modal03"), document.getElementById("modal04"), document.getElementById("modal05"), document.getElementById("modal06"), document.getElementById("modal07"), document.getElementById("modal08"), document.getElementById("modal09"));
+const botoes = [];
+botoes.push(document.getElementById("botao-fechar01"), document.getElementById("botao-fechar02"), document.getElementById("botao-fechar03"), document.getElementById("botao-fechar04"), document.getElementById("botao-fechar05"), document.getElementById("botao-fechar06"), document.getElementById("botao-fechar07"), document.getElementById("botao-fechar08"), document.getElementById("botao-fechar09"));
+
+// laço para escutar clique nos cards específicos, abrir e fechar o modal
+
+for (let i = 0; i < cards.length; i++) {
+
+    cards[i].addEventListener('click', abreModal, false);
+    botoes[i].addEventListener('click', fechaModal, false);
+
+    function abreModal() {
+        modals[i].setAttribute("id", "modal-troca");
+    };
+
+    function fechaModal() {
+        modals[i].removeAttribute("id", "");
+    };
+
+    // fechar modal apertando no esc ou em clicando em qualquer lugar
+
+    modals[i].addEventListener('click', function () {
+        fechaModal();
+    });
+
+    window.onkeydown = function (event) {
+        if (event.keyCode == 27) {
+            modals[0].removeAttribute("id", "");
+            modals[1].removeAttribute("id", "");
+            modals[2].removeAttribute("id", "");
+            modals[3].removeAttribute("id", "");
+            modals[4].removeAttribute("id", "");
+            modals[5].removeAttribute("id", "");
+            modals[6].removeAttribute("id", "");
+            modals[7].removeAttribute("id", "");
+            modals[8].removeAttribute("id", "");
         }
-    }
-}
-
-botao.addEventListener('click', fechaModal, false);
-
-function fechaModal() {
-    modalCard[0].classList.toggle('modal-troca')
-}
+    };
+};
